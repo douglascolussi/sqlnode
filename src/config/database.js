58e93179df2d0ee830bfc65e1fa638008e9b3e7b
douglas <douglascolussi@hotmail.com>
2020-11-18 09:06:58 -0400

    primeiro commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'locahost',
+    username: 'root',
+    passowrd: '',
+    database: 'sqlnode',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
